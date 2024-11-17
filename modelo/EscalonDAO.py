08f84ca64a74b98f3b818873f31b4f8d5babfce7
Kadir <kadiruriel@gmail.com>
2024-11-17 11:01:21 -0300

    cambie self.bd.commit por cursor.connection.commit()

--- modelo/EscalonDAO.py
+++ modelo/EscalonDAO.py
@@ -3,6 +3,11 @@ from psycopg2.extras import RealDictCursor
 from .bd import Database
 from Escalon import Escalon
 
+##############Estructura en la tabla################
+# escalon_partida = nro_escalon(pk) + estado + id_tema
+#################################################################
+
+
 class EscalonDAO:
     def __init__(self):
         self.__bd = Database()
@@ -18,17 +23,17 @@ class EscalonDAO:
         with self.__bd.cursor() as cursor:
             query = "INSERT INTO escalon (id_tema, estado) VALUES (%s, %s) RETURNING nro_escalon"
             cursor.execute(query, (escalon.get_tema(), escalon.get_estado()))
-            self.connection.commit()
+            cursor.connection.commit()
             return cursor.fetchone()[0]
 
     def actualizar_escalon(self, escalon):
         with self.__bd.cursor() as cursor:
             query = "UPDATE escalon SET id_tema = %s, estado = %s WHERE nro_escalon = %s"
             cursor.execute(query, (escalon.get_tema(), escalon.get_estado(), escalon.get_nro_escalon()))
-            self.__bd.commit()
+            cursor.connection.commit()
 
     def borrar_escalon(self, nro_escalon):
         with self.__bd.cursor() as cursor:
             query = "DELETE FROM escalon WHERE nro_escalon = %s"
             cursor.execute(query, (nro_escalon,))
-            self.__bd.commit()
+            cursor.connection.commit()
