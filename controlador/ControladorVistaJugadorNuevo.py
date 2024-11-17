39031a7968ea1e13c3e2a65b8b26873f89db99dd
TaniaHernandez1 <taniahernandez026@gmail.com>
2024-11-17 16:29:31 -0300

    commit avance con el buscador de  VistaJugadorNuevo

--- controlador/ControladorVistaJugadorNuevo.py
+++ controlador/ControladorVistaJugadorNuevo.py
@@ -1,5 +1,7 @@
 from vista.VistaJugadorNuevo import Ui_MainWindow
 from PyQt6 import QtWidgets
+from modelo.Jugador import Jugador
+from modelo.JugadorDAO import JugadorDAO
 #from Modelo.Jugador import Jugador
 
 class ControladorVistaJugadorNuevo:
@@ -21,8 +23,10 @@ class ControladorVistaJugadorNuevo:
         
         self.imagen_actual = self.lista_imagenes[0]
         self.__vista.set_label_img(self.imagen_actual)
-        
-        #self.__vista.get_button_agregar_jugador().clicked.connect(self.__agregar_jugador_bd_)
+
+        #Este es el boton que estoy modificando:
+        self.__vista.get_button_agregar_jugador().clicked.connect(self.__agregar_jugador_bd_)
+
         self.__vista.get_button_cancelar().clicked.connect(self.__volver_seleccion_de_jugadores)
         self.__vista.get_boton_deslizador_derecha().clicked.connect(self.__mostrar_siguiente_imagen)
         self.__vista.get_boton_deslizador_izquierda().clicked.connect(self.__mostrar_imagen_anterior)
@@ -31,37 +35,40 @@ class ControladorVistaJugadorNuevo:
         self.MainWindow.close()
         self.__controlador_anterior.MainWindow.show()
     
-        '''    
-    def __agregar_jugador_bd_(self):
-        #Agrega un nuevo jugador y su avatar a la BD,Validandolos
-        try:
-            nombre_jugador=self.__vista.get_entrada_texto()
+
+    def __crear_jugador(self):
+        nombre_jugador = self.__vista.get_entrada_texto()
+        
+        jugador_dao = JugadorDAO()
+        obtengo = jugador_dao.get_all_jugadores #me trae todos los jugadores 
+        
+        if any(jugador_dao[1]==nombre_jugador):
+            self.__vista.aviso_nombre_repetido(nombre_jugador)
+            return 
+        
+        
+        # try:
+        #     nombre_jugador = self.__vista.get_entrada_texto()
             
-            if not nombre_jugador:
-                raise ValueError("El nombre del jugador esta vacio")
-            else:
-                jugador_nuevo=Jugador()
-                jugador_nuevo.agregar_jugador(nombre_jugador)
-                self.__vista.notifico_insercion(nombre_jugador)
-                self.MainWindow.close()
-        except:
-                self.__vista.imprimo_alerta()
-        '''        
+        #     if not nombre_jugador:
+        #         raise ValueError("El nombre del jugador esta vacio")
+        #     else:
+        #         jugador_nuevo = Jugador()
+        #         #comparo el nomnbre
+        #         #jugador_nuevo.agrego_jugador(nombre_jugador)
+        #         #Aca agrego a jugadorDAO
+        #         self.__vista.notifico_insercion(nombre_jugador)
+        #         self.MainWindow.close()
+        # except:
+        #         self.__vista.aviso_nombre_repetido()
 
 
     def __mostrar_siguiente_imagen(self):
-        #inidice y lista
-        #va al metodo siguiente
-        #if not i = len(self.lista)
-        # i = self.lista_imagenes[0]
-        # i += 1
-        # self.__vista.set_label_img(self.lista_imagenes[i])
         if len(self.imagen_actual) > 0:
             i = self.lista_imagenes.index(self.imagen_actual)
             i = (i + 1) % len(self.lista_imagenes)
             self.__vista.set_label_img(self.lista_imagenes[i])
             self.imagen_actual = self.lista_imagenes[i]
-
 
     def __mostrar_imagen_anterior(self):
         if len(self.lista_imagenes) > 0:
