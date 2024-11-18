8d95b8f06c10d7bd2a612d43d4ccedb2510b89c5
ranipe-facundo <ranipe_facundo@hotmail.com>
2024-11-18 13:51:31 -0300

    cambio jugadores en partida

--- modelo/Partida.py
+++ modelo/Partida.py
@@ -1,8 +1,14 @@
 class Partida():
-    def __init__(self, escalones):
+    def __init__(self, escalones, jugadores):
         self.__escalones = escalones
-    
+        self.__jugadores = jugadores
+
     def set_escalones(self, escalones):
         self.__escalones = escalones
     def get_escalones(self):
         return self.__escalones
+    
+    def set_jugadores(self, jugadores):
+        self.__jugadores = jugadores
+    def get_jugadores(self):
+        return self.__jugadores
