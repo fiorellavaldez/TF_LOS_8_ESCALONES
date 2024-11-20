94d5bb4acca6e290dbc9634bda7ddb7c28d6302f
DayanaLaime <dayanalaime91@gmail.com>
2024-11-20 01:24:23 -0300

    funciona vista de jugador nuevo

--- modelo/JugadoresABM.py
+++ modelo/JugadoresABM.py
@@ -3,7 +3,7 @@ from modelo.Jugador import Jugador
 
 class JugadorABM:
     def __init__(self):
-     self.__lista_jugadores = self.obtener_jugadores()
+        self.__lista_jugadores = self.obtener_jugadores()
     
     @property
     def lista_jugadores(self):
@@ -25,19 +25,19 @@ class JugadorABM:
                 self.lista_jugadores[j].set_nombre(jugador.get_nombre_jugador())
                 self.lista_jugadores[j].set_avatar(jugador.get_avatar())
         JugadorDAO().actualizar_jugador(jugador.get_idjugador(), jugador.get_nombre_jugador(), jugador.get_avatar())
-       
+
 
     def existe_jugador(self, jugador: Jugador):
-        tope =len(self.lista_jugadores)
+        tope =len(self.__lista_jugadores)
         id = 0 
         encontrado = False
         while id < tope and not encontrado:
-            if self.lista_jugadores[id].get_nombre_jugador() == jugador.get_nombre_jugador():
+            if self.__lista_jugadores[id].get_nombre_jugador() == jugador.get_nombre_jugador():
                 encontrado = True
             id +=1
         return encontrado
 
-    def agregar_tema(self, jugador: Jugador):
+    def agregar_jugador(self, jugador: Jugador):
         self.__lista_jugadores.append(jugador)
         JugadorDAO().agregar_jugador(jugador.get_nombre_jugador(), jugador.get_avatar())
 
