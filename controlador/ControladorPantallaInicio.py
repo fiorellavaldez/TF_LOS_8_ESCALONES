3d3dae12639d12fe8f0ff61e8f2aee9ede019b48
Alexis-Mariqueo <91996004+Alexis-Mariqueo@users.noreply.github.com>
2024-11-24 18:06:37 -0300

    musica al juego
    agregue musica al juego

--- controlador/ControladorPantallaInicio.py
+++ controlador/ControladorPantallaInicio.py
@@ -10,10 +10,12 @@ class ControladorPantallaInicio:
         self.MainWindow = QtWidgets.QMainWindow()
         self.__vista = Ui_MainWindow()
         self.__vista.setupUi(self.MainWindow)
-        self.MainWindow.show()
         
         # Registrar la ventana en el controlador de audio y video
         ControladorAudiovideo.registrar_ventana(self.MainWindow)
+        self.MainWindow.show()
+
+
         
         with open("vista/estilos.qss") as f:
             self.MainWindow.setStyleSheet(f.read())
@@ -22,6 +24,18 @@ class ControladorPantallaInicio:
         #self.__vista.get_button_continuar().clicked.connect(self.__continuar)
         self.__vista.get_button_configuracion().clicked.connect(self.__configuracion) #Clickear el boton (Es un evento) ?
         self.__vista.get_button_salir().clicked.connect(self.__salir)
+        
+    
+    def ajustar_volumen_musica(self, volumen):
+        # Cambiar el volumen de la música
+        ControladorAudiovideo.ajustar_volumen(volumen)
+
+    def detener_musica(self):
+        # Detener música desde este controlador
+        if ControladorAudiovideo().music_on:
+            ControladorAudiovideo.alternar_musica()
+        # Registrar la ventana en el controlador de audio y video
+        ControladorAudiovideo.registrar_ventana(self.MainWindow)
         
     def __nueva_partida(self):
         self.MainWindow.close()
