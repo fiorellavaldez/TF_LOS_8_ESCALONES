d966853dc7cf5a9d1a7400171bdeb6ed390df19d
Fiore <fiorellavald3z@gmail.com>
2024-11-24 18:28:55 -0300

    se puede volver al menú
    arreglé lo que rompí jiji

--- controlador/ControladorVistaJuego.py
+++ controlador/ControladorVistaJuego.py
@@ -288,7 +288,8 @@ class ControladorVistaJuego:
         # Obtén los layouts actuales y el siguiente
         layout_actual = getattr(self.__vista, f"ly_escalon{nro + 1}")  # Layout del escalón actual
         layout_siguiente = getattr(self.__vista, f"ly_escalon{nro + 2}", None)  # Layout del próximo escalón
-        layout_siguiente.parent().parent().setStyleSheet("") #"Vacía" el StyleSheet para que use el color del .qss
+        if len(lista_ganadores) > 1:
+            layout_siguiente.parent().parent().setStyleSheet("") #"Vacía" el StyleSheet para que use el color del .qss
 
         # Limpia el layout actual desde el tercer elemento en adelante
         for i in range(2, layout_actual.count()):  # Saltar los primeros dos elementos
