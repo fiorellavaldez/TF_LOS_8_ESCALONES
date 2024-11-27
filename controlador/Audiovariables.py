308ed05e91b0180752befd6015237276a0df081c
Alexis-Mariqueo <91996004+Alexis-Mariqueo@users.noreply.github.com>
2024-11-27 11:38:00 -0300

    falto esto

--- controlador/Audiovariables.py
+++ controlador/Audiovariables.py
@@ -3,5 +3,5 @@
 # Configuraciones de audio
 DEFAULT_SOUND_PATH = r"musica\\menu_2.mp3"  # Ruta al archivo de sonido predeterminado
 DEFAULT_VOLUME = 0.5  # Volumen predeterminado (0.0 a 1.0)
-MUSIC_BUTTON_TEXT_ON = "Encender/Apagar Música"
-MUSIC_BUTTON_TEXT_OFF = "Encender/Apagar Música"
+#MUSIC_BUTTON_TEXT_ON = "Encender/Apagar Música"
+#MUSIC_BUTTON_TEXT_OFF = "Encender/Apagar Música"
